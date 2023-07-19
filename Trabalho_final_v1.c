1f0adcbc12839550969d68fd4f3d84f9112986d2
IgorDeitos <139661887+IgorDeitos@users.noreply.github.com>
2023-07-19 20:25:46 -0300

    Update Trabalho_final_v1.c
    alterei a largura de 1000 para 900, para ficar como multiplo da matriz.
    alterei o switch/case para dentro da função menu()

--- Trabalho_final_v1.c
+++ Trabalho_final_v1.c
@@ -1,17 +1,33 @@
 #include "raylib.h"
 #include <string.h>
 
-#define LARGURA 1000
+#define LARGURA 900
 #define ALTURA 600
 
 void menu(){
-    DrawText("Selecione uma opcao:", 300, 200, 40, BLACK);
 
-    DrawText("N: novo jogo", 300, 300, 40, BLACK);
-    DrawText("C: carregar jogo", 300, 350, 40, BLACK);
-    DrawText("S: salvar jogo", 300, 400, 40, BLACK);
-    DrawText("Q: sair jogo", 300, 450, 40, BLACK);
-    DrawText("V: voltar jogo", 300, 500, 40, BLACK);
+    char op;
+
+    DrawText("Solecione uma opcao:", 250, 100, 40, BLACK);
+
+    DrawText("N: novo jogo", 300, 250, 40, BLACK);
+    DrawText("C: carregar jogo", 300, 300, 40, BLACK);
+    DrawText("S: salvar jogo", 300, 350, 40, BLACK);
+    DrawText("Q: sair jogo", 300, 400, 40, BLACK);
+    DrawText("V: voltar jogo", 300, 450, 40, BLACK);
+
+    menu();
+    scanf(" %c", &op);
+
+    switch(toupper(op)){
+        case 'N': novo_jogo(); break;
+        case 'C': carregar_jogo(); break;
+        case 'S': salvar_jogo(); break;
+        case 'Q': sair_jogo(); break;
+        case 'V': voltar_jogo(); break;
+        default: printf("Erro");
+    }
+
 }
 
 void novo_jogo(){
@@ -34,23 +50,9 @@ int main(void){
     InitWindow(LARGURA, ALTURA, "JOGO");
     SetTargetFPS(60);
 
-    char op;
-
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        //menu();
-        if (IsKeyPressed(KEY_RIGHT)){
-            menu();
-            scanf(" %c", &op);
-            switch(toupper(op)){
-                case 'N': novo_jogo(); break;
-                case 'C': carregar_jogo(); break;
-                case 'S': salvar_jogo(); break;
-                case 'Q': sair_jogo(); break;
-                case 'V': voltar_jogo(); break;
-                default: printf("Erro");
-            }
-        }
+        if (IsKeyPressed(KEY_M))menu();
 
         BeginDrawing();//Inicia o ambiente de desenho na tela
         ClearBackground(RAYWHITE);//Limpa a tela e define cor de fundo
